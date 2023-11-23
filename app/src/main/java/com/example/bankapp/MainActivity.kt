8ecfe308733840caaee66a9e68c941f9c42cc12e
Bharath A L <albharath99@gmail.com>
2023-11-23 23:10:06 +0530

    Added main activity UI and Signup page UI

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.bankapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var login: Button
+    private lateinit var signup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signup = findViewById(R.id.signup)
+        signup.setOnClickListener {
+            val intent = Intent(this, SignupPage::class.java)
+            startActivity(intent)
+        }
     }
 }
