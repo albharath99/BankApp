8b37a0c5225ef791ef895fa65002f93859ad015c
Bharath A L <albharath99@gmail.com>
2023-11-26 17:52:07 +0530

    Added: 1) Login page 2) Local DB: create table, insert user, validate card number

---
+++ app/src/main/java/com/example/bankapp/LoginPage.kt
@@ -0,0 +1,32 @@
+package com.example.bankapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginPage : AppCompatActivity() {
+    private lateinit var login: Button
+    private lateinit var cardNumberEt: EditText
+    private lateinit var db: LocalDataBaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+
+        db = LocalDataBaseHelper(applicationContext)
+        login = findViewById(R.id.login2)
+        cardNumberEt = findViewById(R.id.cardNumber2)
+
+        login.setOnClickListener {
+            val cardNumber = cardNumberEt.text.toString().toInt()
+            if(db.loginUser(cardNumber)) {
+                Toast.makeText(applicationContext,"Card number exists", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(applicationContext,"Card number does not exist", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
